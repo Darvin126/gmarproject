fd4eb19ab4b814e052bee54545778e3f56c86a7d
darvin126 <ellaytz@gmail.com>
2022-11-10 10:37:39 +0200

    shalom

--- app/src/main/java/com/example/gmarproject/MainActivity.java
+++ app/src/main/java/com/example/gmarproject/MainActivity.java
@@ -10,5 +10,24 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Thread splashScreenStarter = new Thread() {
+            public void run() {
+                try {
+                    int delay = 0;
+                    while (delay &lt; 2000) {
+                        sleep(150);
+                        delay = delay + 100;
+                    }
+                    startActivity(new Intent(splashscreen.this, MainActivity.class));
+
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                } finally {
+                    finish();
+                }
+            }
+
+        };
+        splashScreenStarter.start();
     }
 }
