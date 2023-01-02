bee820d20d1cf994d1f359dff1d4739faf0354b5
Cyber Lab <>
2023-01-02 15:27:00 +0200

    יםםנש

--- app/src/main/java/com/example/gmarproject/contract.java
+++ app/src/main/java/com/example/gmarproject/contract.java
@@ -1,6 +1,8 @@
 package com.example.gmarproject;
 
 import android.annotation.SuppressLint;
+import android.os.StrictMode;
+import android.util.Log;
 
 import java.sql.Connection;
 
@@ -9,8 +11,18 @@ public class contract {
       Connection con;
       @SuppressLint("NewApi")
         public Connection conclass(){
-          String ip = "172.1.2.0", port = "1433", dp =
+          String ip = "172.1.2.0", port = "1433", dp = , username+"", password= "";
 
+          StrictMode.ThreadPolicy a = new StrictMode.ThreadPolicy.Builder().permitAll().build();
+          StrictMode.setThreadPolicy(a);
+          String ConnectURL = null;
+          try{
+              class.for
+          }
+          catch (Exception e){
+              Log.e("Error is ", e.getMessage());
+
+          }
         }
     }
 }
