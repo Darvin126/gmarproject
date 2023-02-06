6d29ba564a7b25a788dcb089b5567b3d1f966746
Cyber Lab <>
2023-02-06 13:43:58 +0200

    יםםנש

--- app/src/main/java/com/example/gmarproject/contract.java
+++ app/src/main/java/com/example/gmarproject/contract.java
@@ -6,23 +6,4 @@ import android.util.Log;
 
 import java.sql.Connection;
 
-public class contract {
-    public final class consql {
-      Connection con;
-      @SuppressLint("NewApi")
-        public Connection conclass(){
-          String ip = "172.1.2.0", port = "1433", dp = , username+"", password= "";
 
-          StrictMode.ThreadPolicy a = new StrictMode.ThreadPolicy.Builder().permitAll().build();
-          StrictMode.setThreadPolicy(a);
-          String ConnectURL = null;
-          try{
-              class.for
-          }
-          catch (Exception e){
-              Log.e("Error is ", e.getMessage());
-
-          }
-        }
-    }
-}
