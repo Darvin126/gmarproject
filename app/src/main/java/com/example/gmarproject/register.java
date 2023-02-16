48087b6efb74d792236adb999de897f50cd17794
Talmid <ellaytz@gmail.com>
2023-02-16 10:48:57 +0200

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/AndroidManifest.xml
    #	app/src/main/java/com/example/gmarproject/contract.java
    #	app/src/main/java/com/example/gmarproject/login.java
    #	app/src/main/res/layout/activity_login.xml

---
+++ app/src/main/java/com/example/gmarproject/register.java
@@ -0,0 +1,21 @@
+package com.example.gmarproject;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class register extends AppCompatActivity implements View.OnClickListener {
+Button reg;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register);
+    }
+
+    @Override
+    public void onClick(View view) {
+
+    }
+}
